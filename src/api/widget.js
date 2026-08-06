b7bef1b3d40e0f23cdf588a6bd0632482e55b77f
Anand <anand02ok@gmail.com>
2026-08-06 17:55:06 +0530

    Initial frontend commit

--- src/api/widget.js
+++ src/api/widget.js
@@ -3,3 +3,4 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const createGuestTicket = (data) => axios.post(`${API_URL}/tickets/guest`, data);
+const widgetBaseUrl = "https://desk-pilot-frontend.vercel.app";
